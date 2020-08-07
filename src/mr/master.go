72f3896c4190889de50669e0fdd92b2b708a34ff
Saurabh Banka <saurabh.banka@gmail.com>
2020-08-07 12:43:00 +0530

    Completed Lab 1

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,18 +1,87 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
 type Master struct {
 	// Your definitions here.
-
+	mapTasks    map[int]Task
+	reduceTasks map[int]Task
+	timers      map[int]*time.Timer
+	lock        sync.Mutex
+	wg          sync.WaitGroup
 }
 
+var MapChan chan int
+var ReduceChan chan int
+
 // Your code here -- RPC handlers for the worker to call.
+
+func (m *Master) GetWork(args *bool, reply *Task) error {
+	for mapID := range MapChan {
+		m.lock.Lock()
+		*reply = m.mapTasks[mapID]
+		m.timers[mapID] = time.AfterFunc(time.Second*10,
+			func() {
+				m.lock.Lock()
+				MapChan <- m.mapTasks[mapID].Index
+				m.lock.Unlock()
+				m.wg.Done()
+			})
+		m.lock.Unlock()
+		m.wg.Add(1)
+		return nil
+	}
+	m.wg.Wait()
+	for reduceID := range ReduceChan {
+		m.lock.Lock()
+		*reply = m.reduceTasks[reduceID]
+		m.timers[reduceID] = time.AfterFunc(time.Second*10,
+			func() {
+				m.lock.Lock()
+				ReduceChan <- m.reduceTasks[reduceID].Index
+				m.lock.Unlock()
+			})
+		m.lock.Unlock()
+		return nil
+	}
+	*reply = Task{Index: -1}
+	return nil
+}
+
+func (m *Master) MapDone(args *Task, reply *bool) error {
+	index := (*args).Index
+	m.lock.Lock()
+	m.timers[index].Stop()
+	delete(m.mapTasks, index)
+	if len(m.mapTasks) == 0 {
+		close(MapChan)
+	}
+	m.lock.Unlock()
+	m.wg.Done()
+	*reply = true
+	return nil
+}
+
+func (m *Master) ReduceDone(args *Task, reply *bool) error {
+	index := (*args).Index
+	m.lock.Lock()
+	m.timers[index].Stop()
+	delete(m.reduceTasks, index)
+	if len(m.reduceTasks) == 0 {
+		close(ReduceChan)
+	}
+	m.lock.Unlock()
+	*reply = true
+	return nil
+}
 
 //
 // an example RPC handler.
@@ -23,7 +92,6 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -46,12 +114,10 @@ func (m *Master) server() {
 // if the entire job has finished.
 //
 func (m *Master) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	m.lock.Lock()
+	result := len(m.mapTasks) == 0 && len(m.reduceTasks) == 0
+	m.lock.Unlock()
+	return result
 }
 
 //
@@ -60,10 +126,37 @@ func (m *Master) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
+	m := Master{
+		mapTasks:    make(map[int]Task),
+		reduceTasks: make(map[int]Task),
+		timers:      make(map[int]*time.Timer),
+	}
 
 	// Your code here.
+	MapChan = make(chan int, len(files))
+	ReduceChan = make(chan int, nReduce)
 
+	for i, file := range files {
+		task := Task{
+			IsMapTask: true,
+			File:      file,
+			Index:     i,
+			NReduce:   nReduce,
+		}
+		m.mapTasks[i] = task
+		MapChan <- i
+	}
+	for i := 0; i < 10; i++ {
+		task := Task{
+			IsMapTask: false,
+			Index:     i,
+			NReduce:   nReduce,
+		}
+		m.reduceTasks[i] = task
+		ReduceChan <- i
+	}
+	// close(MapChan)
+	// close(ReduceChan)
 
 	m.server()
 	return &m
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,17 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"path/filepath"
+	"sort"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,6 +20,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,18 +39,153 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
-	// Your worker implementation here.
+	for true {
+		args := true
+		reply := Task{}
+		success := call("Master.GetWork", &args, &reply)
 
-	// uncomment to send the Example RPC to the master.
-	// CallExample()
+		if !success || reply.Index == -1 {
+			os.Exit(0)
+		}
+		if reply.IsMapTask {
+			if !doMap(reply, mapf) {
+				fmt.Println("Map Task failed %v\n", reply.Index)
+			}
+		} else {
+			if !doReduce(reply, reducef) {
+				fmt.Println("Reduce Task failed %v\n", reply.Index)
+			}
+		}
+	}
+}
 
+func doMap(task Task, mapf func(string, string) []KeyValue) bool {
+	content := readFile(task.File)
+	intermediate := mapf(task.File, string(content))
+
+	// creating #NReduce files to store output of Map function
+	ofiles := make(map[int]*os.File)
+	for i := 0; i < task.NReduce; i++ {
+		tname := fmt.Sprintf("mr-out-%d-%d", task.Index, i)
+		ofile, err := os.Create(tname)
+		if err != nil {
+			log.Fatalf("Error opening file %v: %v\n", tname, err)
+		}
+		ofiles[i] = ofile
+	}
+
+	// writing output of map to correct file based on hash of key
+	for _, kv := range intermediate {
+		reduceTaskNo := ihash(kv.Key) % task.NReduce
+		ofile := ofiles[reduceTaskNo]
+		enc := json.NewEncoder(ofile)
+		err := enc.Encode(&kv)
+		if err != nil {
+			log.Fatalf("Error encoding file: %v", err)
+		}
+	}
+
+	// closing the files
+	for _, ofile := range ofiles {
+		err := ofile.Close()
+		if err != nil {
+			log.Fatalf("Error closing file %v: %v\n", ofile.Name(), err)
+		}
+	}
+
+	reply := false
+	if call("Master.MapDone", &task, &reply) {
+		return reply
+	}
+	return false
+}
+
+func doReduce(task Task, reducef func(string, []string) string) bool {
+	oname := fmt.Sprintf("mr-out-%v", task.Index)
+	ofile, err := os.Create(oname)
+	if err != nil {
+		log.Fatalf("Error opening file %v: %v\n", oname, err)
+	}
+
+	// getting list of files for current reduce task
+	filePattern := fmt.Sprintf("mr-out-*-%v", task.Index)
+	files, err := filepath.Glob(filePattern)
+	if err != nil {
+		fmt.Printf("Error finding files with pattern %v: %v", filePattern, err)
+		return false
+	}
+
+	// reading output of Map task and appending to intermediate
+	intermediate := []KeyValue{}
+	for _, filename := range files {
+		content := readFile(filename)
+		dec := json.NewDecoder(bytes.NewReader(content))
+		for dec.More() {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				fmt.Printf("Error decoding file %v kv %v: %v", filename, kv, err)
+			}
+			intermediate = append(intermediate, kv)
+		}
+	}
+
+	// sorting and performing Reduce task on similar keys and storing in correct file
+	sort.Sort(ByKey(intermediate))
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	err = ofile.Close()
+	if err != nil {
+		log.Fatalf("Error closing file %v: %v\n", oname, err)
+	}
+
+	// deleting intermediate files created by Map task
+	for _, filename := range files {
+		err = os.Remove(filename)
+		if err != nil {
+			log.Fatalf("Error removing file %v: %v\n", filename, err)
+		}
+	}
+
+	reply := false
+	if call("Master.ReduceDone", &task, &reply) {
+		return reply
+	}
+	return false
+}
+
+func readFile(filename string) []byte {
+	file, err := os.Open(filename)
+	file.Seek(0, 0)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Fatalf("cannot close %v", filename)
+	}
+	return content
 }
 
 //
